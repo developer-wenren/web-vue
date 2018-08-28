d7de5d040b23a7e67bd34550ef4980f6ea9ab49a
wrcj12138aaa <wrcj12138@163.com>
2018-08-28 08:26:29 +0800

    - 添加热更新依赖

--- src/main/java/com/one/webvue/Controller/IndexController.java
+++ src/main/java/com/one/webvue/Controller/IndexController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("eventName", "FIFA 2018");
+        model.addAttribute("eventName", "FIFA 2015");
         return "index";
     }
 }
