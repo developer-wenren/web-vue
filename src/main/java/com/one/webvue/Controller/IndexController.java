da2e5fa6f0fe46ff448978dd33358ccc21d9b3b2
wrcj12138aaa <wrcj12138@163.com>
2018-08-28 08:20:34 +0800

    - SpringBoot 与 Vue

---
+++ src/main/java/com/one/webvue/Controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.one.webvue.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+- 
+
+- Coding By One ON 08/28
+*/
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("eventName", "FIFA 2018");
+        return "index";
+    }
+}
---
+++ src/main/java/com/one/webvue/WebVueApplication.java
@@ -0,0 +1,12 @@
+package com.one.webvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebVueApplication.class, args);
+	}
+}
